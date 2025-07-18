8b546acfbf1672821594b38758560b3b2db45e66
Luca <lucamuller2004@gmail.com>
2025-07-18 19:00:26 +0200

    clicker.component.ts aktualisieren

--- zahlenraten-game/src/app/components/clicker/clicker.component.ts
+++ zahlenraten-game/src/app/components/clicker/clicker.component.ts
@@ -1,1 +1,18 @@
-ja
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-clicker',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './clicker.component.html',
+  styleUrls: ['./clicker.component.scss']
+})
+export class ClickerComponent {
+  money = 0;
+
+  click() {
+    this.money += 1;
+  }
+}

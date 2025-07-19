e0dd15efc1ff22c9b19ba9e2f7a9c4bc7a6c3288
Luca <lucamuller2004@gmail.com>
2025-07-19 23:17:59 +0200

    card-shop.component.ts erstellen

---
+++ zahlenraten-game/src/app/components/card-shop/card-shop.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-card-shop',
+  templateUrl: './card-shop.component.html',
+  styleUrls: ['./card-shop.component.scss']
+})
+export class CardShopComponent {
+  money = 30; // ← später dynamisch laden
+  message = '';
+
+  cardPacks = [
+    { name: 'Basic', price: 10, image: 'assets/packs/basic.png' },
+    { name: 'Premium', price: 20, image: 'assets/packs/premium.png' },
+    { name: 'Ultra', price: 30, image: 'assets/packs/ultra.png' }
+  ];
+
+  constructor(private router: Router) {}
+
+  buyPack(pack: any) {
+    if (this.money < pack.price) {
+      this.message = '❌ Nicht genug Geld!';
+      return;
+    }
+
+    this.message = '';
+    // ggf. Geld abziehen hier oder beim Öffnen
+    this.router.navigate(['/pack-opening'], { queryParams: { pack: pack.name } });
+  }
+}

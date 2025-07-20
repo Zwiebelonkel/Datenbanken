df5ba4477a7ddd3f48f49c063889dc035afb668c
Luca <lucamuller2004@gmail.com>
2025-07-20 18:38:32 +0200

    Create pack-opening.component.ts

---
+++ zahlenraten-game/src/app/components/pack-opening/pack-opening.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-pack-opening',
+  templateUrl: './pack-opening.component.html',
+  styleUrls: ['./pack-opening.component.scss']
+})
+export class PackOpeningComponent implements OnInit {
+  packName: string = '';
+  result: string = '';
+  reveal = false;
+
+  // Wahrscheinlichkeiten je Pack
+  chances: any = {
+    Basic: [
+      { multiplier: '1.2x', chance: 70 },
+      { multiplier: '1.5x', chance: 25 },
+      { multiplier: '2x', chance: 5 },
+    ],
+    Premium: [
+      { multiplier: '1.5x', chance: 60 },
+      { multiplier: '2x', chance: 30 },
+      { multiplier: '5x', chance: 10 },
+    ],
+    Ultra: [
+      { multiplier: '2x', chance: 50 },
+      { multiplier: '5x', chance: 50 },
+    ]
+  };
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.packName = params['pack'];
+      this.drawCard();
+    });
+  }
+
+  drawCard() {
+    const pack = this.chances[this.packName];
+    const rand = Math.random() * 100;
+    let cumulative = 0;
+
+    for (const entry of pack) {
+      cumulative += entry.chance;
+      if (rand <= cumulative) {
+        this.result = entry.multiplier;
+        break;
+      }
+    }
+
+    // Animation verzögert starten
+    setTimeout(() => this.reveal = true, 1000);
+  }
+}

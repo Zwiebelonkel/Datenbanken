b133b3a970857ff560889aec13f85e6bf88c105c
Luca <lucamuller2004@gmail.com>
2025-07-26 14:16:16 +0200

    sound.service.ts aktualisieren

--- zahlenraten-game/src/app/services/sound.service.ts
+++ zahlenraten-game/src/app/services/sound.service.ts
@@ -5,20 +5,20 @@ export class SoundsService {
   private audioCache: { [key: string]: HTMLAudioElement } = {};
 
   playSound(filename: string, volume: number = 1) {
-    const path = `assets/sounds/${filename}`;
+    //const path = `assets/sounds/${filename}`;
 
     // Cache verwenden, um mehrfaches Laden zu vermeiden
-    let audio = this.audioCache[filename];
+    //let audio = this.audioCache[filename];
 
-    if (!audio) {
-      audio = new Audio(path);
-      this.audioCache[filename] = audio;
-    }
+    //if (!audio) {
+      //audio = new Audio(path);
+      //this.audioCache[filename] = audio;
+  //  }
 
-    audio.currentTime = 0;
-    audio.volume = volume;
-    audio.play().catch(err => {
-      console.warn(`⚠️ Sound konnte nicht abgespielt werden: ${filename}`, err);
-    });
+    //audio.currentTime = 0;
+    //audio.volume = volume;
+    //audio.play().catch(err => {
+      //console.warn(`⚠️ Sound konnte nicht abgespielt werden: ${filename}`, err);
+   // });
   }
 }

d068673b2ac28fdaa5225c58418dd5ecf5270399
Luca <lucamuller2004@gmail.com>
2025-08-05 18:15:35 +0200

    village.component.ts aktualisieren

--- zahlenraten-game/src/app/components/village/village.component.ts
+++ zahlenraten-game/src/app/components/village/village.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { VillageService } from '../../services/village.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-village',

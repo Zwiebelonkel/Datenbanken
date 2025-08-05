6af5373a3cb5ebbe7a860f17ea2a5206a50e6c57
Luca <lucamuller2004@gmail.com>
2025-08-05 18:10:04 +0200

    village.service.ts aktualisieren

--- zahlenraten-game/src/app/services/village.service.ts
+++ zahlenraten-game/src/app/services/village.service.ts
@@ -1,18 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthService } from './auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class VillageService {
   private apiUrl = '/api/collect';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private auth: AuthService) {}
 
   collectIncome() {
-    const token = localStorage.getItem('token');
+    const token = this.auth.getToken();
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-    return this.http.get<{ earned: number; minutesPassed: number }>(
-      this.apiUrl,
-      { headers }
-    );
+    return this.http.get<{ earned: number; minutesPassed: number }>(this.apiUrl, { headers });
   }
 }

306229465f226080109d836b265ccfe556544096
Luca <lucamuller2004@gmail.com>
2025-08-06 18:46:59 +0200

    Update chat.component.ts

--- zahlenraten-game/src/app/components/chat/chat.component.ts
+++ zahlenraten-game/src/app/components/chat/chat.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 interface ChatMessage {
   username: string;

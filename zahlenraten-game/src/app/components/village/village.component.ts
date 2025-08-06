57c9b2860f0470778a9d728ed9691a60036ff10c
Luca <lucamuller2004@gmail.com>
2025-08-06 16:31:13 +0200

    Update village.component.ts

--- zahlenraten-game/src/app/components/village/village.component.ts
+++ zahlenraten-game/src/app/components/village/village.component.ts
@@ -101,8 +101,8 @@ export class VillageComponent implements OnInit, AfterViewInit, OnDestroy {
             x: homeX,
             y: homeY,
             state: 'goingToMine',
-            targetX: this.mine.x + (i % 2) * 10,
-            targetY: this.mine.y + Math.floor(i % 2) * 10,
+            targetX: this.mine.x + 20
+            targetY: this.mine.y + 20
             workTimer: 0,
             restTimer: 0,
             homeX,

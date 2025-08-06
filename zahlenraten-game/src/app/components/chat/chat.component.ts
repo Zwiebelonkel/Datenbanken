2af7fc0992e045c18523681b93c13f99393b067d
Luca <lucamuller2004@gmail.com>
2025-08-06 18:34:05 +0200

    chat.component.ts erstellen

---
+++ zahlenraten-game/src/app/components/chat/chat.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface ChatMessage {
+  username: string;
+  message: string;
+  created_at: string;
+}
+
+@Component({
+  selector: 'app-global-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.scss']
+})
+export class ChatComponent implements OnInit {
+  messages: ChatMessage[] = [];
+  newMessage = '';
+  username = 'Gpt'; // Optional: aus AuthService holen
+  loading = false;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadMessages();
+    setInterval(() => this.loadMessages(), 5000);
+  }
+
+  loadMessages() {
+    this.http.get<ChatMessage[]>('/api/chat/latest').subscribe(data => {
+      this.messages = data;
+    });
+  }
+
+  sendMessage() {
+    if (!this.newMessage.trim()) return;
+
+    const msg = this.newMessage;
+    this.newMessage = '';
+    this.http.post('/api/chat/send', {
+      username: this.username,
+      message: msg
+    }).subscribe(() => {
+      this.loadMessages();
+    });
+  }
+}

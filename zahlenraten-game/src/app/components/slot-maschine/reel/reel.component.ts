ea0a9330075a30fe686cf2654ef082403836cd78
Luca <lucamuller2004@gmail.com>
2025-08-08 13:57:16 +0200

    Update reel.component.ts

--- zahlenraten-game/src/app/components/slot-maschine/reel/reel.component.ts
+++ zahlenraten-game/src/app/components/slot-maschine/reel/reel.component.ts
@@ -7,14 +7,27 @@ import { Component, Input } from '@angular/core';
   standalone: true
 })
 export class ReelComponent {
-  @Input() symbol: string = '❔';
+  @Input() symbols: string[] = ['🍒', '🍋', '🔔', '💎', '🍀'];
+  @Input() finalSymbol: string = '❔';
 
+  currentSymbol: string = '❔';
   spinning = false;
+  private intervalId: any;
 
   spin() {
     this.spinning = true;
+    
+    // Schnelles Symbolwechsel-Intervall starten (z.B. alle 50ms)
+    this.intervalId = setInterval(() => {
+      const randomIndex = Math.floor(Math.random() * this.symbols.length);
+      this.currentSymbol = this.symbols[randomIndex];
+    }, 50);
+
+    // Nach 1 Sekunde Spin beenden, finale Symbol setzen
     setTimeout(() => {
+      clearInterval(this.intervalId);
+      this.currentSymbol = this.finalSymbol;
       this.spinning = false;
-    }, 1000); // Dauer der Animation in ms
+    }, 1000);
   }
 }

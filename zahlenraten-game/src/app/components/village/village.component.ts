2d156c562a8914e0912e0b5e5be4bcf65b34f79c
Luca <lucamuller2004@gmail.com>
2025-08-08 11:26:03 +0200

    Update village.component.ts

--- zahlenraten-game/src/app/components/village/village.component.ts
+++ zahlenraten-game/src/app/components/village/village.component.ts
@@ -14,6 +14,7 @@ import { VillageService } from '../../services/village.service';
 import { ProfileService } from '../../services/profile.service';
 import { AuthService } from '../../services/auth.service';
 import { MoneyService } from '../../services/money.service';
+import {AchievementService } from '../../services/achievement.service';
 
 interface Villager {
   id: number;
@@ -77,7 +78,8 @@ export class VillageComponent implements OnInit, AfterViewInit, OnDestroy {
     private villageService: VillageService,
     private profileService: ProfileService,
     private auth: AuthService,
-    private moneyService: MoneyService
+    private moneyService: MoneyService,
+    private achievementService: AchievementService
   ) {}
 
   ngOnInit() {

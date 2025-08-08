bede945d632711eac0d1a8b7c3439a5fc624c357
Luca <lucamuller2004@gmail.com>
2025-08-08 15:45:17 +0200

    Update reel.component.ts

--- zahlenraten-game/src/app/components/slot-maschine/reel/reel.component.ts
+++ zahlenraten-game/src/app/components/slot-maschine/reel/reel.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-reel',
+  imports: [CommonModule],
   templateUrl: './reel.component.html',
   styleUrls: ['./reel.component.scss'],
   standalone: true

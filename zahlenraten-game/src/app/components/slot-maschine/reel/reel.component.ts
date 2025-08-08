710cc25d33137ebffca40cf52a740a9e65a03513
Luca <lucamuller2004@gmail.com>
2025-08-08 13:31:18 +0200

    Update reel.component.ts

--- zahlenraten-game/src/app/components/slot-maschine/reel/reel.component.ts
+++ zahlenraten-game/src/app/components/slot-maschine/reel/reel.component.ts
@@ -1,13 +1,20 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-reel',
   templateUrl: './reel.component.html',
   styleUrls: ['./reel.component.scss'],
-  standalone: true,
-  imports: [CommonModule]
+  standalone: true
 })
 export class ReelComponent {
   @Input() symbol: string = '❔';
+
+  spinning = false;
+
+  spin() {
+    this.spinning = true;
+    setTimeout(() => {
+      this.spinning = false;
+    }, 1000); // Dauer der Animation in ms
+  }
 }

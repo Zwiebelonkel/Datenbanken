5753d7d3c5a9d4e033cb1ee022d117084a3fdcac
Luca <lucamuller2004@gmail.com>
2025-08-08 11:24:21 +0200

    Update village.component.ts

--- zahlenraten-game/src/app/components/village/village.component.ts
+++ zahlenraten-game/src/app/components/village/village.component.ts
@@ -61,8 +61,7 @@ export class VillageComponent implements OnInit, AfterViewInit, OnDestroy {
   unsavedEarnings = 0;
   editingVillagerId: number | null = null;
   newName = '';
-  achievementMessage = ''
-
+  achievementMessage: string | null = null;
   villageLevel = 1;
   villagers: VillagerAnim[] = [];
   incomePerMinute = 0;

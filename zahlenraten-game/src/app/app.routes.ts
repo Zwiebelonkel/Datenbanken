7bd837a0c789b121995b71f8542b6baf2154ebec
Luca <lucamuller2004@gmail.com>
2025-08-08 13:08:44 +0200

    Update app.routes.ts

--- zahlenraten-game/src/app/app.routes.ts
+++ zahlenraten-game/src/app/app.routes.ts
@@ -10,6 +10,7 @@ import { ClickerComponent } from './components/clicker/clicker.component';
 import { CardShopComponent } from './components/card-shop/card-shop.component';
 import { PackOpeningComponent } from './components/pack-opening/pack-opening.component';
 import { VillageComponent } from './components/village/village.component';
+import { SlotMachineComponent } from './components/slot-machine/slot-machine.component';
 
 export const routes: Routes = [
   { path: '', component: GameComponent },
@@ -24,4 +25,5 @@ export const routes: Routes = [
   { path: 'card-shop', component: CardShopComponent },
   { path: 'pack-opening', component: PackOpeningComponent },
   { path: 'village', component: VillageComponent },
+  { path: 'slot-machine', component: SlotMachineComponent },
 ];

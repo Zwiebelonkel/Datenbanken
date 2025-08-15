196a187db7e24bea7572209cab88e3e6e4a44ab4
Luca <lucamuller2004@gmail.com>
2025-08-15 13:38:14 +0200

    Update skill-shop.component.ts

--- zahlenraten-game/src/app/components/skill-shop/skill-shop.component.ts
+++ zahlenraten-game/src/app/components/skill-shop/skill-shop.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common'; // Wichtig für *ngFor, currency
-import { Skill, Player } from './skill.model';
+import { Skill, Player } from './skill.model'; // Dein Skill-Model importieren
 
 @Component({
   selector: 'app-skill-shop',

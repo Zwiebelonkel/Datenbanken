73b63f1f7b9b80bcd6f6640c81ee8c9092942819
Luca <lucamuller2004@gmail.com>
2025-08-15 13:33:32 +0200

    Update skill.model.ts

--- zahlenraten-game/src/app/components/skill-shop/skill.model.ts
+++ zahlenraten-game/src/app/components/skill-shop/skill.model.ts
@@ -3,5 +3,13 @@ export interface Skill {
   name: string;
   description: string;
   price: number;
+  type: 'score' | 'money'; // Typ des Multiplikators: Score oder Monetary
+  level: number;            // Level des Multiplikators (1, 2, 3, ...)
   purchased: boolean;
+}
+
+export interface Player {
+  skillPoints: number;
+  scoreMultiplier: number;
+  monetaryMultiplier: number;
 }

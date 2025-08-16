34170dc42204cf9d8aa436e7587030b31651bcb9
Luca <lucamuller2004@gmail.com>
2025-08-16 12:22:35 +0200

    Update skill-shop.component.ts

--- zahlenraten-game/src/app/components/skill-shop/skill-shop.component.ts
+++ zahlenraten-game/src/app/components/skill-shop/skill-shop.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService, Skill, UserStats } from '../../services/profile.service';
-import { AuthService } from '../../services/auth.serivce'
+import { AuthService } from '../../services/auth.service'
 import { CommonModule } from '@angular/common';
 
 @Component({

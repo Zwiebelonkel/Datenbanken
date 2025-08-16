1427ee6140c0edf0b1c9670721bc869c34ed03f2
Luca <lucamuller2004@gmail.com>
2025-08-16 13:49:42 +0200

    server.js aktualisieren

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use("/api/scores", scoresRoutes);
 app.use("/api/profile", profileRoutes);
 app.use('/api/skills', skillRoutes);
-app.use("api/money", moneyRoutes);
+app.use("/api/money", moneyRoutes);
 app.use("/api/cards", cardsRoutes);
 app.use("/api/village", villageRoutes);
 app.use("/api/chat", chatRoutes);

460f42f2e4ea0ebe10edfa3298e0ff94dfffecbf
Luca <lucamuller2004@gmail.com>
2025-08-20 11:35:14 +0200

    Update main.ts

--- zahlenraten-game/src/main.ts
+++ zahlenraten-game/src/main.ts
@@ -4,10 +4,12 @@ import { provideRouter } from '@angular/router';
 import { importProvidersFrom } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { routes } from './app/app.routes'; // falls du Routing hast
+import { FirebaseService } from './app/services/firebase.service'; // <--- hinzugefügt
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(HttpClientModule) // <-- GANZ WICHTIG: HttpClientModule
+    importProvidersFrom(HttpClientModule),
+    FirebaseService // Initialisiert Firebase automatisch beim App-Start
   ]
 });

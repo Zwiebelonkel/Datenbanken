19b1eaa86f59de27e153869227291ff3bfb46370
Luca <lucamuller2004@gmail.com>
2025-08-20 13:41:17 +0200

    Update achievements.component.ts

--- zahlenraten-game/src/app/components/achievements/achievements.component.ts
+++ zahlenraten-game/src/app/components/achievements/achievements.component.ts
@@ -20,7 +20,7 @@ export class AchievementsComponent implements OnInit {
   constructor(private http: HttpClient, private authService: AuthService, private firebase: FirebaseService) {}
 
   ngOnInit(): void {
-    this.firebase.logEvent(analytics, 'visit_achievements', { debug: true });
+    this.firebase.logEvent('page_visit', { label: 'Achievements' });
     const username = this.authService.getUsername();
     this.http
       .get<any[]>(

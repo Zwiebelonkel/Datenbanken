0e4300760b7b57c5e1dc40a3eab2a5a21e882a8e
Luca <lucamuller2004@gmail.com>
2025-08-20 13:33:46 +0200

    Update app.config.ts

--- zahlenraten-game/src/app/app.config.ts
+++ zahlenraten-game/src/app/app.config.ts
@@ -1,11 +1,14 @@
+// app.config.ts
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { provideRouter, withScrollPositionRestoration } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withScrollPositionRestoration('enabled') // Scrollt automatisch zu (0, 0) nach Navigation
+    ),
   ],
 };

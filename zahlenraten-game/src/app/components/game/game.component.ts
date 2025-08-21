927dc6660e6943fb52aadeb80074a2b5655c8a90
Luca <lucamuller2004@gmail.com>
2025-08-21 17:04:41 +0200

    Update game.component.ts

--- zahlenraten-game/src/app/components/game/game.component.ts
+++ zahlenraten-game/src/app/components/game/game.component.ts
@@ -452,7 +452,7 @@ export class GameComponent implements OnInit {
   }
 
   unlockAchievement(name: string) {
-    if(!authService.isLoggedIn()) return;
+    if(!this.authService.isLoggedIn()) return;
     this.http
       .post<{ unlocked: boolean; name: string }>(
         'https://outside-between.onrender.com/api/unlock',

3e8aa255966fc847a17197429460db5a5e665fb5
MARINESOFT\mueller_ja <jan-luca.mueller@benntec.de>
2025-08-27 14:45:20 +0200

    removedunnessaryImport

--- zahlenraten-game/src/app/components/offline/offline.component.ts
+++ zahlenraten-game/src/app/components/offline/offline.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TopbarComponent } from '../topbar/topbar.component';
 
 @Component({
   selector: 'app-offline',
   standalone: true,
-  imports: [CommonModule, TopbarComponent],
+  imports: [CommonModule],
   templateUrl: './offline.component.html',
   styleUrls: ['./offline.component.scss'],
 })

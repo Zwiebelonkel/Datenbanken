8cb5a0457dcd88a2897fc66dae290745fdeac051
MARINESOFT\mueller_ja <jan-luca.mueller@benntec.de>
2025-09-22 10:19:23 +0200

    Made Consent delayed

--- zahlenraten-game/src/app/app.component.ts
+++ zahlenraten-game/src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ViewContainerRef,
+  ComponentRef,
+  inject,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AuthService } from './services/auth.service';
@@ -10,18 +17,15 @@ import { FirebaseService } from './services/firebase.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterModule,
-    FooterComponent,
-    OfflineComponent,
-    ConsentDialogComponent,
-    CommonModule,
-  ],
+  imports: [RouterModule, FooterComponent, OfflineComponent, CommonModule],
   templateUrl: './app.component.html',
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   isBootstrapping = true;
   isOffline = false;
+
+  private vcr = inject(ViewContainerRef);
+  private consentDialogRef: ComponentRef<ConsentDialogComponent> | null = null;
 
   constructor(
     private auth: AuthService,
@@ -33,6 +37,18 @@ export class AppComponent implements OnInit {
       next: (_ok: boolean) => (this.isBootstrapping = false),
       error: () => (this.isBootstrapping = false),
     });
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.showConsentDialog();
+    }, 100);
+  }
+
+  showConsentDialog() {
+    if (!localStorage.getItem('cookieConsent')) {
+      this.consentDialogRef = this.vcr.createComponent(ConsentDialogComponent);
+    }
   }
 
   logout() {

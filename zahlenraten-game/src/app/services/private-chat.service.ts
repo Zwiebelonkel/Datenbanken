5a75a3491656f0f08bf706dec125682dc06a1206
lucamuller2004 <lucamuller2004@gmail.com>
2025-09-24 11:43:06 +0000

    pls

--- zahlenraten-game/src/app/services/private-chat.service.ts
+++ zahlenraten-game/src/app/services/private-chat.service.ts
@@ -1,8 +1,9 @@
 
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { io } from 'socket.io-client';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,20 +13,30 @@ export class PrivateChatService {
   private socket;
   private apiUrl = 'https://outside-between.onrender.com/api';
 
-  constructor(private http: HttpClient) {
-    this.socket = io('https://outside-between.onrender.com');
+  constructor(private http: HttpClient, private authService: AuthService) {
+    const token = this.authService.getToken();
+    this.socket = io('https://outside-between.onrender.com', {
+      auth: {
+        token: token
+      }
+    });
+  }
+
+  private getHeaders(): HttpHeaders {
+    const token = this.authService.getToken();
+    return new HttpHeaders().set('Authorization', `Bearer ${token}`);
   }
 
   getUsers(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}/users`);
+    return this.http.get<any[]>(`${this.apiUrl}/users`, { headers: this.getHeaders() });
   }
 
   getMessages(user1: string, user2: string): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}/messages/${user1}/${user2}`);
+    return this.http.get<any[]>(`${this.apiUrl}/messages/${user1}/${user2}`, { headers: this.getHeaders() });
   }
 
   sendMessage(message: any): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/messages`, message);
+    return this.http.post<any>(`${this.apiUrl}/messages`, message, { headers: this.getHeaders() });
   }
 
   onNewMessage(): Observable<any> {

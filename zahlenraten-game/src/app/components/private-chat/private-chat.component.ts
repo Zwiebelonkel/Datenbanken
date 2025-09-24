1f16b4596c84be422e7de9169d7aa98cd6a10917
lucamuller2004 <lucamuller2004@gmail.com>
2025-09-24 12:44:23 +0000

    design

--- zahlenraten-game/src/app/components/private-chat/private-chat.component.ts
+++ zahlenraten-game/src/app/components/private-chat/private-chat.component.ts
@@ -4,13 +4,16 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { PrivateChatService } from '../../services/private-chat.service';
 import { AuthService } from '../../services/auth.service';
+import { LoaderComponent } from '../loader/loader.component';
+import { TopbarComponent } from '../topbar/topbar.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-private-chat',
   templateUrl: './private-chat.component.html',
   styleUrls: ['./private-chat.component.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule]
+  imports: [CommonModule, FormsModule, LoaderComponent, TopbarComponent, SidebarComponent]
 })
 export class PrivateChatComponent implements OnInit {
 
@@ -18,21 +21,28 @@ export class PrivateChatComponent implements OnInit {
   selectedUser: any;
   messages: any[] = [];
   newMessage: string = '';
+  username: string | null = null;
+  isLoading: boolean = false;
 
   constructor(private privateChatService: PrivateChatService, private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.username = this.authService.getUsername();
+    this.isLoading = true;
     this.privateChatService.getUsers().subscribe(users => {
-      this.users = users;
+      this.users = users.filter((u: any) => u.username !== this.username);
+      this.isLoading = false;
     });
   }
 
   selectUser(user: any) {
     this.selectedUser = user;
-    const username = this.authService.getUsername();
-    if (username) {
-        this.privateChatService.getMessages(username, this.selectedUser.username).subscribe(messages => {
+    this.messages = [];
+    if (this.username) {
+        this.isLoading = true;
+        this.privateChatService.getMessages(this.username, this.selectedUser.username).subscribe(messages => {
           this.messages = messages || [];
+          this.isLoading = false;
         });
     }
   }
@@ -42,12 +52,12 @@ export class PrivateChatComponent implements OnInit {
       return;
     }
 
-    const username = this.authService.getUsername();
-    if (username) {
+    if (this.username) {
         const message = {
-          sender: username,
+          sender: this.username,
           receiver: this.selectedUser.username,
-          message: this.newMessage
+          message: this.newMessage,
+          created_at: new Date()
         };
 
         this.privateChatService.sendMessage(message).subscribe(() => {
@@ -55,5 +65,15 @@ export class PrivateChatComponent implements OnInit {
           this.newMessage = '';
         });
     }
+  }
+
+  trackByMsg(index: number, msg: any): any {
+    return msg.created_at; 
+  }
+
+  copyToClipboard(text: string) {
+    navigator.clipboard.writeText(text).then(() => {
+      // Optional: show a notification that text was copied.
+    });
   }
 }
